d84dee0dbaf39a15355d5ee07db13cd6727c197c
raazeshp96 <raazeshp96@gmail.com>
2023-06-30 22:00:45 +0545

    fix:explore next image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { Card, Divider, Spin } from "antd";
+import Image from "next/image";
 import { useCallback, useEffect, useRef } from "react";
 import { useInfiniteQuery } from "react-query";
 import styled from "styled-components";
@@ -90,7 +91,19 @@ const ItemList: React.FC = () => {
    * becomes visible in the viewport
    *
    */
+  const keyStr =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
 
+  const triplet = (e1: number, e2: number, e3: number) =>
+    keyStr.charAt(e1 >> 2) +
+    keyStr.charAt(((e1 & 3) << 4) | (e2 >> 4)) +
+    keyStr.charAt(((e2 & 15) << 2) | (e3 >> 6)) +
+    keyStr.charAt(e3 & 63);
+
+  const rgbDataURL = (r: number, g: number, b: number) =>
+    `data:image/gif;base64,R0lGODlhAQABAPAA${
+      triplet(0, r, g) + triplet(b, 255, 255)
+    }/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`;
   useEffect(() => {
     const element = observerElem.current;
     const option = { threshold: 0 };
@@ -117,13 +130,19 @@ const ItemList: React.FC = () => {
                     <div key={`${index}-${data?.index || 0}`}>
                       <Card
                         hoverable
-                        style={{ width: "350px" }}
+                        style={{ width: "35rem" }}
                         cover={
-                          <img
-                            src={data.images[0]}
+                          <Image
+                            src={
+                              "https://www.shutterstock.com/image-illustration/smart-city-nft-virtual-real-600w-2154735667.jpg"
+                            }
                             width={200}
                             height={200}
-                            style={{ objectFit: "contain", padding: "20px" }}
+                            style={{ objectFit: "contain", padding: "2rem" }}
+                            alt={"image "}
+                            loading="lazy"
+                            placeholder="blur"
+                            blurDataURL={rgbDataURL(237, 181, 6)}
                           />
                         }
                         title={data.title || ""}
@@ -135,8 +154,8 @@ const ItemList: React.FC = () => {
                       </Card>
                       <Divider
                         style={{
-                          marginTop: "10px",
-                          marginBottom: "10px",
+                          marginTop: "1rem",
+                          marginBottom: "1rem",
                         }}
                       />
                     </div>
@@ -145,7 +164,7 @@ const ItemList: React.FC = () => {
               </Container>
             );
           })}
-          <div className={"loader"} ref={observerElem}>
+          <div ref={observerElem}>
             {isFetchingNextPage && hasNextPage ? (
               <div
                 style={{
